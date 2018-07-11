ec50609c53d74528e3f542891ca49439f22315aa
Aniruddh Gopale <anigopale@gmail.com>
2018-07-12 00:25:48 +0530

    render spinner/loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header, Card } from './components/common';
+import { Header, Card, Spinner, CardSection } from './components/common';
 import axios from 'axios';
 import Search from './components/Search';
 import StockList from './components/StockList';
 
 export default class App extends Component {
 
-  state = { term: '', stocks: [] };
+  state = { term: '', stocks: [], loading: true };
 
   componentWillMount() {
     this.fetchData();
@@ -21,15 +21,33 @@ export default class App extends Component {
       filter = `?SYMBOL_like=${term}`;
     }
 
-    this.setState({ stocks: [] });
+    // before fetching toggle loading flag
+    this.setState({ stocks: [], loading: true });
+
     // fetch data and store in app state
     axios.get(`${url}${filter}`)
     .then(response => {
-      this.setState({ stocks: response.data });
+      this.setState({ stocks: response.data, loading: false });
     })
     .catch(error => {
       console.log(error);
+      this.setState({ stocks: [], loading: true });
     });
+  }
+
+  renderStockList() {
+    // loading flat is true, render spinner
+    if (this.state.loading) {
+      return (
+        <CardSection>
+          <Spinner />
+        </CardSection>
+      );
+    }
+
+    return (
+      <StockList stocks={this.state.stocks} />
+    );
   }
 
   render() {
@@ -38,7 +56,7 @@ export default class App extends Component {
         <Header headerText="Stock App" />
         <Card>
           <Search getSearchTerm={this.fetchData} />
-          <StockList stocks={this.state.stocks} />
+          {this.renderStockList()}
         </Card>
       </View>
     );
